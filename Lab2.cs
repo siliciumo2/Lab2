829dfcc5d3ff335df1af9857716dc458a6c3a6e0
Siliciumo2 <147613900+siliciumo2@users.noreply.github.com>
2023-10-24 10:12:02 +0300

    Add files via upload

---
+++ Lab2.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите a,b,c");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int minus = 0;
+    if (a < 0)
+{
+        minus++;
+}
+    if (b < 0)
+{
+    minus++;
+}
+    if (c < 0)
+{
+    minus++;
+}
+Console.WriteLine($"Отрицательные числа - {minus}");
